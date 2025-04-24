c89f5eab155a7adf74780c9e55e43a2d02551163
Jainil-Patel <jainilp4321@gmail.com>
2025-04-24 23:23:22 +0530

    cors error fix

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoute');
 const blogRoutes = require('./routes/blogRoute');
-
+const cors = require('cors')
 const app = express();
 const PORT = process.env.PORT || 5000
 app.use(express.json());
-
+app.use(cors())
 app.use('/api/auth/', authRoutes);
 app.use('/api/blogs', blogRoutes);
 
